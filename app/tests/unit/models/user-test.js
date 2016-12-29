495c9d808adf7344351e87251b4999376581ea0f
Christian Muehlhaeuser <muesli@gmail.com>
2016-12-29 07:21:46 +0100

    Removed tests for invitation model and added user model tests instead

--- app/tests/unit/models/user-test.js
+++ app/tests/unit/models/user-test.js
@@ -1,6 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('invitation', 'Unit | Model | invitation', {
+moduleForModel('user', 'Unit | Model | invitation', {
   // Specify the other units that are required for this test.
   needs: []
 });
