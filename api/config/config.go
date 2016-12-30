6b9d5f79f6d6f8a37dd7a395810239cf26424555
Christian Muehlhaeuser <muesli@gmail.com>
2016-12-30 04:34:45 +0100

    Updated config package

--- api/config/config.go
+++ api/config/config.go
@@ -1,8 +1,11 @@
-package main
+package config
 
 import (
 	"errors"
 	"flag"
+	"reflect"
+	"strconv"
+	"strings"
 
 	log "github.com/Sirupsen/logrus"
 )
@@ -13,68 +16,83 @@ type ConfigData struct {
 		BaseURL         string
 		PathPrefix      string
 		Bind            string
-		CryptPepper     string
 		SwaggerAPIPath  string
 		SwaggerPath     string
 		SwaggerFilePath string
+	}
+
+	Connections struct {
+		Email                EmailConfig
+		PostgreSQLConnection PostgreSQLConnection
+		Logging              struct {
+			IrcHost       string
+			IrcChannel    string
+			IrcImpChannel string
+		}
 	}
 
 	Web struct {
 		BaseURL string
 	}
 
-	Connections struct {
-		Email struct {
-			AdminEmail string
-			ReplyTo    string
-			SMTP       struct {
-				User     string
-				Password string
-				Server   string
-				Port     int
-			}
-			IMAP struct {
-				User        string
-				Password    string
-				Server      string
-				Port        int
-				LastMessage string
-				LastSeen    int64
-			}
-		}
-		PostgreSQLConnection PostgreSQLConnection
-	}
+	App struct {
+		CryptPepper string
 
-	Proposals struct {
-		SmallGrantThreshold uint
-	}
-
-	Templates struct {
-		Invitation struct {
-			Subject string
-			Text    string
-			HTML    string
+		Proposals struct {
+			SmallGrantThreshold uint
 		}
-		ModerationProposal struct {
-			Subject string
-			Text    string
-			HTML    string
-		}
-	}
 
-	Logging struct {
-		IrcHost       string
-		IrcChannel    string
-		IrcImpChannel string
+		Templates Templates
+	}
+}
+
+// PostgreSQLConnection contains all of the db configuration values
+type PostgreSQLConnection struct {
+	User     string
+	Password string
+	Host     string
+	DbName   string
+	SslMode  string
+}
+
+// EmailTemplate holds all values of an email template
+type EmailTemplate struct {
+	Subject string
+	Text    string
+	HTML    string
+}
+
+type Templates struct {
+	Invitation         EmailTemplate
+	ModerationProposal EmailTemplate
+}
+
+type EmailConfig struct {
+	AdminEmail string
+	ReplyTo    string
+	SMTP       struct {
+		User     string
+		Password string
+		Server   string
+		Port     int
+	}
+	IMAP struct {
+		User        string
+		Password    string
+		Server      string
+		Port        int
+		LastMessage string
+		LastSeen    int64
 	}
 }
 
 var (
 	configHandler *ConfigHandler
-	config        *ConfigData
+	Settings      *ConfigData
 )
 
-func parseSettings() {
+// ParseSettings parses the config file
+func ParseSettings() {
 	logLevelStr := flag.String("loglevel", "info", "Log level")
 	configFile := flag.String("configfile", "config.json", "config file in the JSON format")
 	flag.Parse()
@@ -101,6 +119,38 @@ func parseSettings() {
 			*configFile,
 		).Fatal(errors.New("Did not get a valid config file"))
 	}
-	config = configHandler.CurrentData().(*ConfigData)
+
+	Settings = configHandler.CurrentData().(*ConfigData)
 	//FIXME catch empty conf fields
+}
+
+// Marshal returns a "Connection String" with escaped values of all non-empty
+// fields as described at http://www.postgresql.org/docs/current/static/libpq-connect.html#LIBPQ-CONNSTRING
+func (c *PostgreSQLConnection) Marshal() string {
+	val := reflect.ValueOf(c).Elem()
+	var out string
+	l := val.NumField()
+
+	r := strings.NewReplacer(`'`, `\'`, `\`, `\\`)
+
+	for i := 0; i < l; i++ {
+		var fieldValue string
+
+		switch f := val.Field(i).Interface().(type) {
+		case string:
+			fieldValue = f
+		case int:
+			fieldValue = strconv.Itoa(f)
+		}
+		fieldType := val.Type().Field(i).Name
+
+		if len(fieldValue) > 0 {
+			out += strings.ToLower(fieldType) + "='" + r.Replace(fieldValue) + "'"
+			if i < l {
+				out += " "
+			}
+		}
+	}
+
+	return out
 }
