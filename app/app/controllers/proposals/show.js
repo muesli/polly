b6b8f7585c871e5db37da9222b24bd8aaf08279c
Christian Muehlhaeuser <muesli@gmail.com>
2017-01-12 14:50:23 +0100

    Show whether the current user already voted on a proposal

--- app/app/controllers/proposals/show.js
+++ app/app/controllers/proposals/show.js
@@ -5,6 +5,19 @@ const { inject: { service } } = Ember;
 export default Ember.Controller.extend({
   currentUser: service('current-user'),
 
+  currentUserVoted: Ember.computed('vote.@each.moderated', function() {
+      const proposalID = this.get('proposal').get('id');
+      var found = false;
+
+      this.get('vote').forEach(function(entry) {
+          if (entry.get('proposal').get('id') === proposalID) {
+              found = true;
+              return;
+          }
+      });
+      return found;
+  }),
+
   actions: {
     moderate(id) {
         this.store.findRecord('proposal', id).then(function(proposal) {
--- app/app/routes/proposals/show.js
+++ app/app/routes/proposals/show.js
@@ -2,7 +2,15 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model(params) {
-    return this.get('store').findRecord('proposal', params.proposal_id);
-  }
+    model(params) {
+       return Ember.RSVP.hash({
+         proposal: this.store.findRecord('proposal', params.proposal_id),
+         vote: this.store.findAll('vote')
+       });
+     },
+
+     setupController(controller, models) {
+       controller.set('proposal', models.proposal);
+       controller.set('vote', models.vote);
+     }
 });
