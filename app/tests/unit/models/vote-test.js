1e3a80ec3f171c9ed292a3dd18e58d5928978b18
Christian Muehlhaeuser <muesli@gmail.com>
2017-01-12 16:42:23 +0100

    Vote has a dependency on Proposal

--- app/tests/unit/models/vote-test.js
+++ app/tests/unit/models/vote-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('vote', 'Unit | Model | vote', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:proposal']
 });
 
 test('it exists', function(assert) {
