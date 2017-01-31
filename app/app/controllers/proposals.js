3be89ebe763322cdd3c7c75044ad8b09a0c9e58e
Christian Muehlhaeuser <muesli@gmail.com>
2017-01-31 15:49:38 +0100

    Update budget model and display period-end in proposals template. Fixes #6

--- app/app/controllers/proposals.js
+++ app/app/controllers/proposals.js
@@ -3,12 +3,18 @@ import moment from 'moment';
 
 export default Ember.Controller.extend({
     maxmicrobudget: 0,
+    periodend: "",
+
+    periodEnd: Ember.computed('periodend', function() {
+        return this.periodend;
+    }),
 
     maxBudget: Ember.computed('maxmicrobudget', function() {
         this.store.query('budget', {
             month: moment().add(14, 'd').toDate().getMonth() + 1
         }).then((budget) => {
             this.set('maxmicrobudget', budget.objectAt(0).get('value'));
+            this.set('periodend', budget.objectAt(0).get('period_end'));
         });
 
         return this.get('maxmicrobudget');
--- app/app/models/budget.js
+++ app/app/models/budget.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  value: DS.attr('number')
+  value: DS.attr('number'),
+  maxvalue: DS.attr('number'),
+  period_end: DS.attr('isodate')
 });
