fc746dad5b3a3478424aacb629e2f938d25f156a
Christian Muehlhaeuser <muesli@gmail.com>
2017-01-31 17:00:49 +0100

    Pre-fill contact info with current user's email

--- app/app/routes/proposals/new.js
+++ app/app/routes/proposals/new.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
+const { inject: { service } } = Ember;
+
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+    currentUser: service('current-user'),
+
     model() {
        return Ember.RSVP.hash({
            budget: this.store.findAll('budget')
@@ -10,6 +14,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
      setupController(controller, models) {
        controller.set('budget', models.budget);
+       controller.set('contact', this.get('currentUser').get('user').get('email'));
        controller.set('errorMessage', '');
        controller.set('responseMessage', '');
        controller.set('progressMessage', '');
