6dc86f24904859b09372530a316503aacd978849
Christian Muehlhaeuser <muesli@gmail.com>
2017-02-02 00:43:30 +0100

    Rename formatTime to format_time

--- app/app/helpers/format-time.js
+++ app/app/helpers/format-time.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 import moment from 'moment';
 
-export function formatTime(params/*, hash*/) {
+export function format_time(params/*, hash*/) {
     return moment(params[0]).tz(moment.tz.guess()).format(params[1]);
 }
 
-export default Ember.Helper.helper(formatTime);
+export default Ember.Helper.helper(format_time);
